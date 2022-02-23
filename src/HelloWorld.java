ed34a5baa15e9ed1bea521f02f44e1f71410758b
hemmerlinaj20 <hemmerlinaj20@gcc.edu>
2022-02-23 14:46:08 -0500

    prints name

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,10 +2,14 @@ import java.util.Scanner;
 
 public class HelloWorld {
     public static void main(String[] args) {
+
         System.out.println("Hello World");
-        Scanner scnr = new Scanner(System.in);
+
         System.out.println("Give me your name.");
+        Scanner scnr = new Scanner(System.in);
         String name = scnr.next();
+
+        System.out.println("Your name is: " + name);
 
     }
 }
