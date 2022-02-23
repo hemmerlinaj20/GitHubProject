f55e56b198fbb2eba9b77c0d87f6b95492850eea
hemmerlinaj20 <hemmerlinaj20@gcc.edu>
2022-02-23 14:36:17 -0500

    Added Scanner

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,11 @@
+import java.util.Scanner;
+
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        Scanner scnr = new Scanner(System.in);
         System.out.println("Give me your name.");
+        String name = scnr.next();
+
     }
 }
